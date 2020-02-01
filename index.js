e4c5f76bd9e9bb14ed32bb67e09cb6c7384fdc0a
lobana alarabi <loubanaeng@gmail.com>
2020-02-01 19:57:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ const record = [
   {year: "2016", result: "N/A"} ]
 function superbowlWin(record)
 {
-    record.find(function(result){return (result==="W")});
+    record.find(function(re){return (result==="W")});
 }
